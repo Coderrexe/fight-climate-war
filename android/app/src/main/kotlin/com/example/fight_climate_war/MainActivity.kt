49a8b7a97a00dae6d3af8b6159d705f9abc434f6
Henry-Hart <wolf3141@icloud.com>
2022-10-01 20:26:40 +0100

    Initial test
    Test for cross-compatibility please

---
+++ android/app/src/main/kotlin/com/example/fight_climate_war/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fight_climate_war
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'src/locations.dart' as locations;
+
+// STAGE 1:
+
+// TODO: - fix BottomTabBar bugs
+// TODO: - organise stuff into files + organise classes
+// TODO: - add actual searching functionality
+// TODO: - set up backend (we can already parse JSON from a google example)
+// TODO: - let user add locations + information
+// TODO: - make something prettier happen when user clicks locations
+// TODO: - make main page & styling
+// TODO: - comments (sorry)
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SearchPage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SearchPage> createState() => _SearchPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class _SearchPageState extends State<SearchPage> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the MainPage widget.
+        //'/': (context) => const MainPage(),
+        // When navigating to the "/second" route, build the SecondRoute widget.
+        '/second': (context) => const SecondRoute(),
+
+      },
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Search Google Locations...'),
+            backgroundColor: Colors.green[700],
+
+          ),
+          body: Scaffold(
+            body: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(0, 0),
+                zoom: 2,
+              ),
+              markers: _markers.values.toSet(),
+            ),
+            bottomNavigationBar: DefaultTabController(
+              length: 2,
+              child: _OptionBottomBar(
+              ),
+              initialIndex: 1,
+            ),
+          )
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+    );
+  }
+}
+
+class _OptionBottomBar extends StatelessWidget{
+  const _OptionBottomBar({
+    this.fabLocation = FloatingActionButtonLocation.endDocked,
+  });
+
+  final FloatingActionButtonLocation fabLocation;
+
+  static final List<FloatingActionButtonLocation> centerLocations =
+  <FloatingActionButtonLocation>[
+    FloatingActionButtonLocation.centerDocked,
+    FloatingActionButtonLocation.centerFloat,
+  ];
+
+  //late final _tabController = TabController(length: 3, vsync: this);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.blue,
+      child: IconTheme(
+        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+        child: TabBar( //TabBar at bottom of appbar
+          onTap: (index){
+            if(index == 0 && !Navigator.canPop(context)) {
+              Navigator.pushNamed(context, '/second');
+            }
+            if(index == 1 && Navigator.canPop(context)) {
+              Navigator.pop(context);
+              //SearchPage.
+            }
+          },
+          tabs: [
+            Tab(icon: Icon(Icons.home)),
+            Tab(icon: Icon(Icons.search))
+            //more tabs here
           ],
         ),
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("This will be the main page"),
+        backgroundColor: Colors.green,
+      ), // AppBar
+      body: Scaffold(
+        body: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Back!'),
+            ), // ElevatedButton
+          ),
+        bottomNavigationBar: DefaultTabController(
+        length: 2,
+        child: _OptionBottomBar(
+          ),
+            initialIndex: 0
+        ),
+      ), // Center
+    ); // Scaffold
   }
 }
